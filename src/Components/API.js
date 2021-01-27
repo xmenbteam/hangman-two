71506334b2ab64c299927ddf0734bc6900b4957f
Sam Parry <samparrymusic@gmail.com>
2021-01-27 17:13:12 +0000

    sorts out API call

---
+++ src/Components/API.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const getRandomMovie = () => {
+  return axios
+    .get(
+      "https://api.themoviedb.org/3/discover/movie?api_key=5253becb732a744099e02486dbac043a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+    )
+    .then((res) => {
+      console.log(res.data.results, "RESPONSE");
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err, "ERROR");
+    });
+};
+
+export default getRandomMovie;
